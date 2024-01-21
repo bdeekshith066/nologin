02396aef75cd9d7be4d6d85f5f50dd103765be71
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-01-22 00:15:19 +0530

    Update generate_link.js

--- generate_link.js
+++ generate_link.js
@@ -1,20 +1,15 @@
-// generateLink.js
-
 function generateLink() {
     const userInput = document.getElementById("userInput").value;
     const durationHours = document.getElementById("durationHours").value;
     const durationMinutes = document.getElementById("durationMinutes").value;
 
-    // Validate inputs (you may want to add more robust validation)
     if (!userInput || !durationHours || !durationMinutes) {
         alert("Please fill in all fields.");
         return;
     }
 
-    // Generate a unique link based on the user input and current timestamp
-    const timestamp = Date.now();
-    const randomNumber = Math.floor(Math.random() * 10000);
-    const link = `https://nologin.com/${userInput}/${timestamp}${randomNumber}`;
+    // Generate a unique link based on the user input
+    const link = `http://localhost:3000/${userInput}`;
 
     // Redirect to the new page (content.html)
     window.location.href = link;
